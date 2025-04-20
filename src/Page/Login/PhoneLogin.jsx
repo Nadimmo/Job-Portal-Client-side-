cd29fdf7df73a5cd2957da8543caa0ecc1dee6ea
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-04-20 20:06:24 +0600

    send user data in database

--- src/Page/Login/PhoneLogin.jsx
+++ src/Page/Login/PhoneLogin.jsx
@@ -54,7 +54,7 @@ function PhoneLogin() {
                         onChange={(e) => setOtp(e.target.value)}
                         className="border p-2 w-full mt-4 mb-3"
                     />
-                    <button onClick={handleVerifyOtp} className="bg-pupal-500 text-white px-4 py-2 rounded w-full cursor-pointer">
+                    <button onClick={handleVerifyOtp} className="bg-blue-500 text-white px-4 py-2 rounded w-full cursor-pointer">
                         Verify OTP
                     </button>
                 </>
--- src/Page/Register/Register.jsx
+++ src/Page/Register/Register.jsx
@@ -4,10 +4,12 @@ import { AuthContext } from '../../AuthProvider/AuthProvider';
 import Swal from 'sweetalert2';
 import { FcGoogle } from 'react-icons/fc';
 import { FaFacebook } from 'react-icons/fa';
+import useAxiosPublic from '../../Components/Hooks/useAxiosPublic';
 
 function Register() {
-    const { signUp, profileUpdate, googleSignIn,  facebookSignIn } = useContext(AuthContext);
+    const { signUp, profileUpdate, googleSignIn, facebookSignIn } = useContext(AuthContext);
     const navigate = useNavigate()
+    const axiosPublic = useAxiosPublic()
     // Assuming you have a useAuth hook to access the AuthContext]
 
     const [formData, setFormData] = useState({
@@ -20,7 +22,6 @@ function Register() {
     });
 
     const [errors, setErrors] = useState({});
-    const [successMessage, setSuccessMessage] = useState('');
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -70,26 +71,62 @@ function Register() {
         e.preventDefault();
 
         const name = formData.firstName + ' ' + formData.lastName;
+        const email = formData.email;
+        const type = formData.userType
+        const userInfo = {
+            name,
+            email,
+            type
+        }
 
         if (validateForm()) {
             // In a real application, you would make an API call here to register the user
             signUp(formData.email, formData.password)
-
                 .then((userCredential) => {
                     if (userCredential.user) {
                         profileUpdate(name)
                             .then((res) => {
-                                setSuccessMessage('Registration successful! Please check your email to verify.');
-                                setFormData({
-                                    firstName: '',
-                                    lastName: '',
-                                    email: '',
-                                    password: '',
-                                    confirmPassword: '',
-                                    userType: 'jobseeker',
-                                });
-                                setErrors({});
-                                navigate('/')
+                                //send data in database
+                                axiosPublic.post('/users', userInfo)
+                                    .then(res => {
+                                        if (res.data.insertedId) {
+                                            // Show success message
+                                            Swal.fire({
+                                                title: "Registration Successful",
+                                                text: "You have successfully registered!",
+                                                icon: "success",
+                                                confirmButtonText: "OK",
+                                            })
+                                            setFormData({
+                                                firstName: '',
+                                                lastName: '',
+                                                email: '',
+                                                password: '',
+                                                confirmPassword: '',
+                                                userType: 'jobseeker',
+                                            });
+                                            setErrors({});
+                                            navigate('/')
+                                        } else {
+                                            Swal.fire({
+                                                title: "Registration Successful",
+                                                text: "You have successfully registered!",
+                                                icon: "success",
+                                                confirmButtonText: "OK",
+                                            })
+                                            setFormData({
+                                                firstName: '',
+                                                lastName: '',
+                                                email: '',
+                                                password: '',
+                                                confirmPassword: '',
+                                                userType: 'jobseeker',
+                                            });
+                                            setErrors({});
+                                            navigate('/')
+                                        }
+                                    })
+
                             })
                     }
                 })
@@ -135,30 +172,7 @@ function Register() {
             });
     }
 
-    // const handleFacebook = () => {
-    //     facebookSignIn()
-    //         .then((result) => {
-    //             const name = result.user.displayName;
-    //             const photoURL = result.user.photoURL;
 
-    //             profileUpdate(name, photoURL)
-    //                 .then(res => {
-    //                     setSuccessMessage('Facebook sign-in successful!');
-    //                     navigate('/')
-    //                 })
-
-    //         })
-    //         .catch((error) => {
-    //             const errorCode = error.code;
-    //             const errorMessage = error.message;
-    //             console.error('Error during Facebook sign-in:', errorCode, errorMessage);
-    //             Swal.fire({
-    //                 icon: 'error',
-    //                 title: 'Facebook Sign-In Failed',
-    //                 text: errorMessage,
-    //             });
-    //         });
-    // }   
 
 
     return (
@@ -175,12 +189,6 @@ function Register() {
                         </Link>
                     </p>
                 </div>
-
-                {successMessage && (
-                    <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-                        <span className="block sm:inline">{successMessage}</span>
-                    </div>
-                )}
 
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
@@ -287,18 +295,13 @@ function Register() {
                         </div>
                         {errors.userType && <p className="text-red-500 text-xs italic">{errors.userType}</p>}
                     </div>
-                    <div  className='divider'> or sign in with </div>
+                    <div className='divider'> or sign in with </div>
                     {/* Social Logins */}
-                    <div className="lg:flex justify-between  gap-4
-                    ">
+                    <div className="lg:flex justify-between  gap-4">
                         <button onClick={handleGoogle} className="flex items-center justify-center w-full py-2 border rounded-lg hover:bg-gray-100 transition cursor-pointer">
                             <FcGoogle
                                 className="mr-2 text-2xl" />
                             Sign in with Google
-                        </button>
-                        <button className="flex items-center justify-center w-full py-2 border rounded-lg hover:bg-gray-100 transition cursor-pointer lg:mt-0 mt-4">
-                            <FaFacebook className="mr-2 text-blue-500 text-2xl" />
-                            Sign in with Facebook
                         </button>
                     </div>
                     <br />
@@ -309,10 +312,6 @@ function Register() {
                         >
                             Sing Up
                         </button>
-                        {/* You can add a link to login here if needed */}
-                        {/* <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-              Already have an account?
-            </a> */}
                     </div>
                 </form>
             </div>
