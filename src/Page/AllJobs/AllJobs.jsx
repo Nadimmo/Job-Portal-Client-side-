033723877040bbb6cfd581af74fe255cce7c0de3
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-04-24 00:29:44 +0600

    update all job page and add save job functionality

--- src/Page/AllJobs/AllJobs.jsx
+++ src/Page/AllJobs/AllJobs.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import useAllJobs from '../../Components/Hooks/useAllJobs';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import Swal from 'sweetalert2';
+import useAxiosPublic from '../../Components/Hooks/useAxiosPublic';
+import { FaBookmark } from 'react-icons/fa';
 
 const AllJobs = () => {
 
     const { allJobs } = useAllJobs()
+    const {user} = useContext(AuthContext)
+    const navigate = useNavigate()
+    const axiosPublic = useAxiosPublic()
+
     const [searchLocation, setSearchLocation] = useState('');
     const [searchType, setSearchType] = useState('');
     const [currentPage, setCurrentPage] = useState(1)
@@ -27,6 +35,36 @@ const AllJobs = () => {
         if (pageNumber < 1 || pageNumber > totalPages) return;
         setCurrentPage(pageNumber);
     }
+
+    const handleSave = async (job) => {
+        if (!user) {
+          Swal.fire({
+            icon: 'error',
+            title: 'You must be logged in to save any job',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          return navigate("/login")
+        } else {
+          const jobInfo = {
+            title: job.title,
+            location: job.location,
+            type: job.type,
+            email: user?.email
+          }
+          try {
+            const response = await axiosPublic.post("/savedJobs", jobInfo);
+            if (response.data) {
+              Swal.fire({
+                icon: "success",
+                title: "Saved job successfully!",
+              });
+            }
+          } catch (error) {
+            console.error("Error saving job:", error.message);
+          }
+        }
+      }
 
 
 
@@ -67,13 +105,17 @@ const AllJobs = () => {
                             }`}>
                             {job.type}
                         </span>
+                        {/* save job button */}
+                        <button onClick={()=>handleSave(job)} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 transition cursor-pointer">
+                            <FaBookmark className="w-5 h-5" />
+                        </button>
                         <div className="flex flex-col items-center text-center">
                             <div className="w-16 h-16 bg-gray-200 rounded-full mb-4"></div>
                             <h3 className="text-lg font-semibold">{job.title}</h3>
                             <p className="text-gray-500 text-sm">{job.location}</p>
                             {/* You can open the modal using document.getElementById('ID').showModal() method */}
                             {/* Apply Button */}
-                            <Link to={`/showJobDetails/${job._id}`} 
+                            <Link to={`/showJobDetails/${job._id}`}
                                 className="mt-4 px-4 py-2 border border-green-500 text-green-500 rounded hover:cursor-pointer hover:text-black hover:bg-green-500  transition"
                             >
                                 Show Details
